a64a746fd4f15beb37cb72d1f4692ca9ee4c9da2
Angel Badillo <badilloa022402@gmail.com>
2022-11-15 21:27:37 -0600

    done

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 // Mask for segment number
 const unsigned int segment_mask = 0x3FF;
-// Mask for unique page number (includes segment number)
+
+// Mask for unique page number
 const unsigned int page_mask = 0x7FF;
 
 int main()
@@ -29,7 +30,7 @@ int main()
     unordered_set<unsigned int> segments;
     unordered_set<unsigned int> pages;
 
-    // Loop until end of file
+    // Loop until end of file, read code and address
     while (infile >> hex >> code >> address)
     {
         // Get unique page number
@@ -41,14 +42,11 @@ int main()
         // Store unique values into sets
         segments.insert(segmentNum);
         pages.insert(pageNum);
-        
-         // See results of bit manipulations
-         cout << hex << address << ',' << segmentNum << ',' << pageNum << '\n';
     }
 
     // Print the number of different segments and pages
-    // cout << dec << "Number of segments: " << segments.size() << '\n';
-    // cout << dec << "Number of pages: " << pages.size() << '\n';
+    cout << dec << "Number of segments: " << segments.size() << '\n';
+    cout << dec << "Number of pages: " << pages.size() << '\n';
     
     // Close file stream
     infile.close();
